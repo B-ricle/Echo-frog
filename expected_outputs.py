580333b93f2ba183df37b526913b46fd230baa3e
B-ricle <bnhenderson1106@gmail.com>
2026-08-04 18:38:58 -0400

    Added: problems.py and finished sandbox.v1

---
+++ expected_outputs.py
@@ -0,0 +1,20 @@
+problems = {
+    "easy": {
+        "description": "Given a sorted list and a target, return the index of the target or -1 if not found. Example: nums = [1, 3, 5, 7, 9, 11], target = 7 -> 3",
+        "input": "nums = [1, 3, 5, 7, 9, 11], target = 7",
+        "expected": "3"
+    },
+
+    "medium": {
+        "description": "Given a sorted list of integers (which may contain duplicates) and a target value, return the index of the first occurrence of the target. Return -1 if it's not present.",
+        "input": "nums = [1, 2, 2, 2, 3, 4, 5], target = 2",
+        "expected": "1",
+    },
+
+    "hard": {
+        "description": "Given a list of distinct integers originally sorted ascending but rotated at an unknown pivot (e.g., [4,5,6,7,0,1,2]), and a target value, return its index using an O(log n) algorithm. Return -1 if not present.",
+        "input": "nums = [4, 5, 6, 7, 0, 1, 2], target = 0",
+        "expected": "4"
+    }
+
+}
--- sandbox.py
+++ sandbox.py
@@ -3,6 +3,7 @@ from docker.errors import NotFound, APIError
 import tarfile
 import io
 import threading
+from expected_outputs import problems
 
 
 class SandboxExecutionTimeout(Exception):
@@ -11,6 +12,7 @@ class SandboxExecutionTimeout(Exception):
 
 #connect to the local docker daemon
 client = docker.from_env()
+
 
 def run_code(code_to_run: str):
 
@@ -80,13 +82,37 @@ def run_code(code_to_run: str):
 
  #Creates multilined structure
 multi_line = []
+current_difficulty = None
+striped_user_input = None
+submit_typed = "Submit"
 
 while True:
-    user_input = input("Enter Submission (or type 'Submit'): ").strip()
+    user_input = input("Select Problem(easy, medium, or hard (when finished type 'Submit')): ")
+    striped_user_input = user_input.strip().lower()
 
-    if user_input == "Submit":
+    if striped_user_input in problems:
+
+        if striped_user_input != current_difficulty:
+
+            multi_line.clear()
+        current_difficulty = striped_user_input
+
+        print(problems[current_difficulty]["description"])
+        continue
+
+    if striped_user_input == submit_typed.lower():
+        if not current_difficulty:
+            print("Error: You must select a problem before submitting.")
+            continue
+        if not multi_line:
+            print("Error: You cannot submit empty code! Please write your solution.")
+            continue
         break
 
+    if not current_difficulty:
+        print("Error: Please select a difficulty('easy', 'medium', or 'hard')")
+        continue
+    
     multi_line.append(user_input)
 
 final_statement = "\n".join(multi_line)
@@ -95,18 +121,19 @@ print(f"final submission: {final_statement} ")
 
 try:
     result = run_code(final_statement)
-    print(result.exit_code)
-    print(result.output.decode("utf-8"))
+    if result.exit_code != 0:
+        print(f"Runtime error:{result.exit_code}")    
+    else:
+        print(result.exit_code)
+        decoded_result = result.output.decode("utf-8")
+
+        print(decoded_result)
+        # Compares the result to the expected output
+        if(decoded_result.strip() == problems[current_difficulty]["expected"]):
+            print("correct")
+        else:
+            print("incorrect")
+
 except SandboxExecutionTimeout as e:
     print("Execution Failed")
     print(e)
-
-        
-
-        
-    
-
-
-    
-
-
